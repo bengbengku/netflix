53087295ed7a433627252da765ddfe82f98772ec
bengbengku <anggiatbenget70@gmail.com>
2020-10-15 06:15:05 +0700

    Setup project #2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Jumbotron from './components/jumbotron/index';
 
 export default function App() {
   return (
-    <p>Hello</p>
+    <Jumbotron/>
   );
 }
 
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Jumbotron() {
+  return (<p>I am the Jumbo!</p>);
+}

05aa61551ad1459c2524a7abe03640419988fa51
bengbengku <anggiatbenget70@gmail.com>
2020-10-15 07:09:40 +0700

    ADD: inisialisasi Jumbotron Container untuk receive data

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Inner, Container } from "./styles/jumbotron";
 
-export default function Jumbotron() {
-  return (<p>I am the Jumbo!</p>);
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Inner direction={direction}>
+      {children}
+    </Inner>
+  );
 }
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};

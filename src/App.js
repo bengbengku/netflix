86f5b41946d0c4cf791d64410c812cbe9449e907
bengbengku <anggiatbenget70@gmail.com>
2020-10-15 18:30:17 +0700

    MODIFY: Clean code untuk jumbotron

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
-import React from 'react';
-import Jumbotron from './components/jumbotron/index';
-import jumboData from './fixtures/jumbo';
+import React from "react";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt}/>
-          </Jumbotron.Pane>
-        </Jumbotron>  
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
-

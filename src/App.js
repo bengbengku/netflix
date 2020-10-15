1928ce6de4133522e1d84e44ea3143cd14b9a135
bengbengku <anggiatbenget70@gmail.com>
2020-10-16 03:51:58 +0700

    ADD: Accordion setup ketika di klik

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
-import {FooterContainer} from "./containers/footer";
+import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
-      <FooterContainer/>
+      <FaqsContainer />
+      <FooterContainer />
     </>
   );
 }
--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,2 +1,24 @@
 import styled from "styled-components/macro";
 
+export const Container = styled.div `
+    display: flex;
+    border-bottom: 8px solid #222;
+`;
+
+export const Inner = styled.div `
+    display: flex;
+    padding: 70px 45px;
+    flex-direction: column;
+    max-width: 815px;
+    margin: auto;
+`;
+
+export const Item = styled.div ``;
+
+export const Header = styled.div ``;
+
+export const Body = styled.div ``;
+
+export const Frame = styled.div ``;
+
+export const Title = styled.h1 ``;

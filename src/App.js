8ff670cf3876b8c7dc1b6035661b726d71440730
bengbengku <anggiatbenget70@gmail.com>
2020-10-16 06:29:32 +0700

    ADD: File pages setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from "./pages";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path="/user">
+        <p>Hello, I am a user</p>
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }

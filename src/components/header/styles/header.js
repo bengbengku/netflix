62c3e613c8c8f7e085555df652bd5ff64709a0d7
bengbengku <anggiatbenget70@gmail.com>
2020-10-16 09:24:09 +0700

    ADD: Fix header dan tombol login

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,31 +1,54 @@
 import styled from "styled-components/macro";
-import { Link as ReactRouterLink} from  "react-router-dom";
+import { Link as ReactRouterLink } from "react-router-dom";
 
-export const Background = styled.div ``;
+export const Background = styled.div``;
 
-export const Frame = styled.div ``;
+export const Frame = styled.div``;
 
-export const Container = styled.div ``;
+export const Container = styled.div`
+  display: flex;
+  margin: 0 56px;
+  height: 64px;
+  padding: 18px 0;
+  justify-content: space-between;
+  align-items: center;
 
-export const Logo = styled.img ``;
+  a {
+      display: flex;
+  }  
 
-export const ButtonLink = styled(ReactRouterLink) `
-    display: block;
-    background-color: #e50914;
-    width: 84px;
-    height: fit-content;
-    color: white;
-    border: 0;
-    font-size: 15px;
-    border-radius: 3px;
-    padding: 8px 17px;
-    cursor: pointer;
-    text-decoration: none;
-    box-sizing: border-box;
-
-    &:hover {
-        background-color: #f40612;
-    }
+  @media (max-width: 1000px) {
+      margin: 0 30px;
+  }
 `;
 
+export const Logo = styled.img`
+  height: 32px;
+  width: 108px;
+  margin-right: 40px;
 
+  @media (max-width: 1449px) {
+    height: 45px;
+    width: 167px;
+  }
+`;
+
+export const ButtonLink = styled(ReactRouterLink)`
+  display: block;
+  background-color: #e50914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
+  transition: all 0.4s ease-in-out;
+
+  &:hover {
+    background-color: #f40612;
+  }
+`;

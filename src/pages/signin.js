72dbb3563b6e91ee8a0002a632cf4f2fcfa3530d
bengbengku <anggiatbenget70@gmail.com>
2020-10-18 00:08:19 +0700

    ADD: Setup form validasi

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,19 +1,34 @@
 import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signin() {
-  const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState('');
+  const history = useHistory();  
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const isInvalid = password === '' || emailAddress === '';
+  const isInvalid = password === "" || emailAddress === "";
   const handleSignin = (event) => {
-      event.preventDefault();
+    event.preventDefault();
 
-      
-  }
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(emailAddress, password)
+    .then(() => {
+        history.push(ROUTES.BROWSE);
+    })
+    .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+    })
+  };
 
   return (
     <>
@@ -40,10 +55,11 @@ export default function Signin() {
             </Form.Submit>
           </Form.Base>
           <Form.Text>
-              New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
           </Form.Text>
           <Form.TextSmall>
-              This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
           </Form.TextSmall>
         </Form>
       </HeaderContainer>

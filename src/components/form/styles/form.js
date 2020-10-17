dba47412e877f6708e806a8bff65c54ef8df326c
bengbengku <anggiatbenget70@gmail.com>
2020-10-18 00:28:24 +0700

    ADD: Modifikasi css info error menjadi ada animasi bergerak

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -29,6 +29,29 @@ export const Error = styled.div`
   margin: 0 0 16px;
   color: white;
   padding: 15px 20px;
+
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+    transform: translate3d(0, 0, 0);
+    backface-visibility: hidden;
+    perspective: 1000px;
+
+    @keyframes shake {
+    10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+    }
+
+    20%, 80% {
+    transform: translate3d(2px, 0, 0);
+    }
+
+    30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+    }
+
+    40%, 60% {
+    transform: translate3d(4px, 0, 0);
+    }
+
 `;
 
 export const Title = styled.h1`
@@ -66,6 +89,7 @@ export const Input = styled.input`
   border: 0;
   color: white;
   height: 50px;
+  outline: none;
   line-height: 50px;
   padding: 5px 20px;
   margin-bottom: 20px;
@@ -76,17 +100,17 @@ export const Input = styled.input`
 `;
 
 export const Submit = styled.button`
-    background: #e50914;
-    border-radius: 4px;
-    font-size: 16px;
-    font-weight: bold;
-    margin: 24px 0 12px;
-    padding: 16px;
-    border: 0;
-    color: white;
-    cursor: pointer;
+  background: #e50914;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 24px 0 12px;
+  padding: 16px;
+  border: 0;
+  color: white;
+  cursor: pointer;
 
-    &:disabled {
-        opacity: 0.5;
-    }
+  &:disabled {
+    opacity: 0.5;
+  }
 `;

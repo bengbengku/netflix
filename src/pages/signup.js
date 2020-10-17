309768eba5c06d4a85f7e900c8f38f9db332d0fc
bengbengku <anggiatbenget70@gmail.com>
2020-10-18 01:11:14 +0700

    ADD: Code mengirim data sign up ke firebase

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -19,6 +19,25 @@ export default function Signup() {
 
   const handleSignup = (event) => {
     event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) => {
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          });
+      }).catch((error) => {
+          setFirstName('');
+          setEmailAddress('');
+          setPassword('');
+          setError(error.message);
+      })
   };
 
   return (

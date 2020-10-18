a389fb1847bb89cdc4bfcd1db53d268a962f7bc5
bengbengku <anggiatbenget70@gmail.com>
2020-10-19 05:08:40 +0700

    ADD + MODIFY: debug browse untuk menangkap data item

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
   const { series } = useContent("series");
-  console.log(series);
+  const { films } = useContent("films");
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
 
   return <p>Hello from the page browse!</p>;
 }
